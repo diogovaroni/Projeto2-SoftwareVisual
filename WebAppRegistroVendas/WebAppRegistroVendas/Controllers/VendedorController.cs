81f91c71ee29434e5543e5ab045887f9befcea8c
Varoni <varonidiogo@gmail.com>
2022-10-03 15:50:13 -0300

    Criação do projeto e do controlador de vendedor

---
+++ WebAppRegistroVendas/WebAppRegistroVendas/Controllers/VendedorController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAppRegistroVendas.Models;
+
+namespace WebAppRegistroVendas.Controllers
+{
+    public class VendedorController : ApiController
+    {
+        // GET: api/Vendedor
+        public IEnumerable<Vendedor> Get()
+        {
+            Vendedor vendedor = new Vendedor();
+
+            return vendedor.ListarVendedores();
+        }
+
+        // GET: api/Vendedor/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Vendedor
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Vendedor/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Vendedor/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebAppRegistroVendas/WebAppRegistroVendas/Models/Vendedor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppRegistroVendas.Models
+{
+    public class Vendedor
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String Email { get; set; }
+        public double BaseSalarial { get; set; }
+
+        public List<Vendedor> ListarVendedores()
+        {
+            Vendedor vendedor = new Vendedor();
+            vendedor.Id = 1;
+            vendedor.Nome = "Diogo";
+            vendedor.Email = "diogo@gmail.com";
+            vendedor.BaseSalarial = 3000;
+
+            List<Vendedor> listaVendedores = new List<Vendedor>();
+            listaVendedores.Add(vendedor);
+
+            return listaVendedores;
+        }
+    }
+}
