48fdc5efe1938786e519065cc0cedcf7afe65333
Varoni <varonidiogo@gmail.com>
2022-10-03 23:06:49 -0300

    Adicionando o CRUD completo na base JSon de Vendedores

--- WebAppRegistroVendas/WebAppRegistroVendas/Models/Vendedor.cs
+++ WebAppRegistroVendas/WebAppRegistroVendas/Models/Vendedor.cs
@@ -24,5 +24,65 @@ namespace WebAppRegistroVendas.Models
             var listaVendedores = JsonConvert.DeserializeObject<List<Vendedor>>(json);
             return listaVendedores;
         }
+
+        public bool ReescreverArquivo(List<Vendedor> listaVendedores)
+        {
+            var caminhoArquivo = HostingEnvironment.MapPath(@"~\App_Data\Vendedores.json");
+
+            var json = JsonConvert.SerializeObject(listaVendedores, Formatting.Indented);
+            File.WriteAllText(caminhoArquivo, json);
+
+            return true;
+        }
+
+        public Vendedor Inserir(Vendedor vendedor)
+        {
+            var listaVendedores = this.ListarVendedores();
+
+            var maxId = listaVendedores.Max(Vendedor => vendedor.Id);
+            vendedor.Id = maxId + 1;
+            listaVendedores.Add(vendedor);
+
+            ReescreverArquivo(listaVendedores);
+            return vendedor;
+        }
+
+        public Vendedor Atualizar(int Id, Vendedor vendedor)
+        {
+            var listaVendedores = this.ListarVendedores();
+
+            var itemIndex = ListarVendedores().FindIndex(p => p.Id == Id);
+            if(itemIndex >= 0)
+            {
+                vendedor.Id = Id;
+                listaVendedores[itemIndex] = vendedor;
+            }
+            else
+            {
+                return null;
+            }
+            ReescreverArquivo(listaVendedores);
+            return vendedor;
+        }
+
+        public bool Deletar(int Id)
+        {
+            var listaVendedores = this.ListarVendedores();
+
+            var itemIndex = listaVendedores.FindIndex(p => p.Id == Id);
+            if (itemIndex >= 0)
+            {
+                listaVendedores.RemoveAt(itemIndex);
+            }
+            else
+            {
+                return false;
+            }
+
+            ReescreverArquivo(listaVendedores);
+            return true;
+        }
+
+
     }
 }
