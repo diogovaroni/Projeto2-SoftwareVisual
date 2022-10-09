6d690b0f2f1acb617b3e8e4cc7f2c8f6788691a9
Varoni <varonidiogo@gmail.com>
2022-10-08 23:17:13 -0300

    Inclusão classe e controller 'Departamento'

---
+++ WebAppRegistroVendas/WebAppRegistroVendas/Controllers/DepartamentoController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAppRegistroVendas.Models;
+
+namespace WebAppRegistroVendas.Controllers
+{
+    public class DepartamentoController : ApiController
+    {
+        // GET: api/Departamento
+        public IEnumerable<Departamento> Get()
+        {
+            Departamento departamento = new Departamento();
+
+            return departamento.ListarDepartamentos();
+        }
+
+        // GET: api/Departamento/Id
+        public Departamento Get(int id)
+        {
+            Departamento departamento = new Departamento();
+            return departamento.ListarDepartamentos().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        // POST: api/Departamento
+        public List<Departamento> Post(Departamento departamento)
+        {
+            Departamento _departamento = new Departamento();
+            _departamento.Inserir(departamento);
+            return _departamento.ListarDepartamentos();
+        }
+
+
+
+
+        // PUT: api/Departamento/Id
+        public Departamento Put(int Id, [FromBody] Departamento departamento)
+        {
+            Departamento _departamento = new Departamento();
+            return _departamento.Atualizar(Id, departamento);
+        }
+
+
+        // DELETE: api/Departamento/Id
+        public void Delete(int Id)
+        {
+            Departamento _departamento = new Departamento();
+            _departamento.Deletar(Id);
+        }
+    }
+}
